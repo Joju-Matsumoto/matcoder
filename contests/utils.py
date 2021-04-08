68733fa4000c6f540930ed1b79439e78cf56eae3
jj_ <avasoldier2@gmail.com>
2021-04-08 17:15:22 +0900

    utils.py datetime -> str

--- contests/utils.py
+++ contests/utils.py
@@ -1,3 +1,7 @@
+import datetime
+
+from django.utils.timezone import localtime
+
 
 day_of_week_to_japanese = {
     "Sun": "日",
@@ -13,3 +17,13 @@ def check_answer(ans1, ans2):
     """ans1とans2が同じかどうかチェックする
     同じ場合はTrue, そうでない場合はFalseを返す"""
     return ans1.strip() == ans2.strip()
+
+def datetime_str(dt: datetime.datetime) -> str:
+    """ datetimeを「21-04-04 (月) 22:04」の形式の文字列で返す
+    """
+    dt = localtime(dt)
+    return "{}({}) {}".format(
+        dt.strftime("%Y-%m-%d"),
+        day_of_week_to_japanese[dt.strftime("%a")],
+        dt.strftime("%H:%M")
+    )
