c7ab7f0939fd4789dd8fedf34f97bc367233eed7
jj_ <avasoldier2@gmail.com>
2021-04-08 17:17:01 +0900

    admin.py TestCase, TestResult

--- contests/admin.py
+++ contests/admin.py
@@ -18,8 +18,16 @@ class ProblemScoreAdmin(admin.ModelAdmin):
 class ContestScoreAdmin(admin.ModelAdmin):
     model = models.ContestScore
 
+class TestCaseAdmin(admin.ModelAdmin):
+    model = models.TestCase
+
+class TestResultAdmin(admin.ModelAdmin):
+    model = models.TestResult
+
 admin.site.register(models.Contest, ContestAdmin)
 admin.site.register(models.Problem, ProblemAdmin)
 admin.site.register(models.Submission, SubmissionAdmin)
 admin.site.register(models.ProblemScore, ProblemScoreAdmin)
 admin.site.register(models.ContestScore, ContestScoreAdmin)
+admin.site.register(models.TestCase, TestCaseAdmin)
+admin.site.register(models.TestResult, TestResultAdmin)
