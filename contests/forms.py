cd5bd5b7bba45abce1a67a6e9afa82b955a8d95f
jj_ <avasoldier2@gmail.com>
2021-04-08 17:15:36 +0900

    forms.py コードテストのフォーム

--- contests/forms.py
+++ contests/forms.py
@@ -14,3 +14,27 @@ class SubmissionForm(forms.ModelForm):
         help_texts = {
             "answer": "解答を入力",
         }
+
+class CodeSubmissionForm(forms.Form):
+    code = forms.CharField(
+        widget=forms.Textarea,
+        help_text="解答欄",
+    )
+    test_input = forms.CharField(
+        widget=forms.Textarea,
+        help_text="サンプル入力",
+        required=False,
+    )
+    test_output = forms.CharField(
+        widget=forms.Textarea,
+        help_text="サンプル出力",
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["code"].widget.attrs["class"] = "form-control"
+        self.fields["test_input"].widget.attrs["class"] = "form-control"
+        self.fields["test_input"].widget.attrs["rows"] = "2"
+        self.fields["test_output"].widget.attrs["class"] = "form-control"
+        self.fields["test_output"].widget.attrs["rows"] = "2"
