c66e462c1d96b71237709a0f149f36c89962131c
jj_ <avasoldier2@gmail.com>
2021-04-08 17:18:10 +0900

    users forms.py css classの追加

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,12 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from . import models
 
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control mt-2"
+            field.widget.attrs["placeholder"] = field.label
+    
     class Meta:
         model = models.User
         fields = ("username", "password1", "password2")
@@ -12,5 +18,5 @@ class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
-            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["class"] = "form-control mb-4"
             field.widget.attrs["placeholder"] = field.label
