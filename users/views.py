8dbd1f4cf25afdc48c4386219b87314a8b2253d4
jj_ <avasoldier2@gmail.com>
2021-04-08 17:18:53 +0900

    users views.py マイページにコンテスト結果を表示したい

--- users/views.py
+++ users/views.py
@@ -3,15 +3,22 @@ from django.contrib.auth import login, authenticate
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views import View
 
 from . import models, forms
 
-# Create your views here.
-def index_view(request):
-    context = {
-        "users": models.User.objects.all()
-    }
-    return render(request, "users/index.html", context)
+# 邪道...
+from contests.models import ContestScore
+
+
+class IndexView(View):
+    def get(self, request):
+        context = {}
+        if request.user.is_authenticated:
+            context["contest_scores"] = ContestScore.objects.filter(user=request.user).order_by("-contest__start_date")
+        return render(request, "users/index.html", context)
+
+index_view = IndexView.as_view()
 
 def signup_view(request):
     if request.method == "POST":
