25ad69d0eeaf4b22c59234002aa95bfcac5ec71c
jj_ <avasoldier2@gmail.com>
2021-04-11 18:09:06 +0900

    forms.py editform, userprofile,

--- contests/forms.py
+++ contests/forms.py
@@ -1,6 +1,89 @@
 from django import forms
 
+from colorfield.widgets import ColorWidget
+
 from . import models
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = models.UserProfile
+
+        fields = ["color", "organization"]
+
+        labels = {
+            "color": "ユーザーカラー",
+            "oorganization": "所属",
+        }
+
+        widgets = {
+            "color": ColorWidget,
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["color"].widget.attrs["cols"] = "7"
+        self.fields["organization"].widget.attrs["style"] = "width: 100%;"
+
+
+class ContestEditForm(forms.ModelForm):
+    """コンテスト編集用のフォーム"""
+    class Meta:
+        model = models.Contest
+        fields = ["abstract", "penalty"]
+
+        labels = {
+            "abstract": "概要",
+            "penalty": "ペナルティ(分)",
+        }
+    
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+
+class ProblemsAddForm(forms.ModelForm):
+    class Meta:
+        model = models.Problem
+        fields = ("order", "title")
+
+
+class ProblemEditForm(forms.ModelForm):
+    """問題編集用のフォーム"""
+    class Meta:
+        model = models.Problem
+        fields = ("order", "title", "question", "answer", "point")
+
+        labels = {
+            "order": "問題順序",
+            "title": "タイトル",
+            "question": "問題文",
+            "answer": "答えのサンプル",
+            "point": "配点",
+        }
+
+
+class TestCaseEditForm(forms.ModelForm):
+    """テストケース 追加・編集 用フォーム"""
+    class Meta:
+        model = models.TestCase
+        fields = ("title", "test_input", "test_output", "is_sample")
+
+        labels = {
+            "title": "タイトル",
+            "test_input": "入力",
+            "test_output": "出力",
+            "is_sample": "これはサンプル",
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key, field in self.fields.items():
+            if key == "is_sample":
+                continue
+            field.widget.attrs["class"] = "form-control"
+        self.fields["test_input"].widget.attrs["rows"] = 4
+        self.fields["test_output"].widget.attrs["rows"] = 4
+
 
 class SubmissionForm(forms.ModelForm):
     """解答提出用のフォーム"""
@@ -14,6 +97,7 @@ class SubmissionForm(forms.ModelForm):
         help_texts = {
             "answer": "解答を入力",
         }
+
 
 class CodeSubmissionForm(forms.Form):
     code = forms.CharField(
