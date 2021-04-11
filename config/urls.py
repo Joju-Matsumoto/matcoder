3dbbfa819db6faae0c37f302dacd1380208f06c2
jj_ <avasoldier2@gmail.com>
2021-04-11 18:12:12 +0900

    delete app_name

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-app_name = "base"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("contests/", include("contests.urls")),
