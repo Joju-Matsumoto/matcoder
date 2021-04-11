5e1ad7e961485dfcaf7c1d987f96cae920a81857
jj_ <avasoldier2@gmail.com>
2021-04-11 18:11:05 +0900

    users models.py userprofileを使う．邪道

--- users/models.py
+++ users/models.py
@@ -2,5 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager
 from django.contrib.auth.base_user import BaseUserManager
 
+from contests.models import UserProfile
+
 class User(AbstractUser):
     objects = UserManager()
+
+    def get_colored_username(self):
+        try:
+            user_profile = UserProfile.objects.get(user=self)
+        except:
+            return self.username
+        return "<span style='color: {};'>{}</span>".format(user_profile.color, self.username)
